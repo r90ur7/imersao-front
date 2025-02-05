04e164dc23ffb09c7ed4eb8cc114a0494a53d4e0
Rallenson <Rallenson90@gmail.com>
2025-02-04 23:14:48 -0300

    Adiciona arquivos de configuração do projeto e aprimora a lógica de busca de artistas

--- search.js
+++ search.js
@@ -1,35 +1,53 @@
+const searchInput = document.getElementById('search-input');
 const resultArtist = document.getElementById("result-artist");
-const playlistContainer = document.getElementById("result-playlists");
-const searchInput = document.getElementById("search-input");
+const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
-  fetch(`http://localhost:3000/artists?name_like=${searchTerm}`)
-    .then((response) => response.json())
-    .then((results) => displayResults(results));
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result, searchTerm));
 }
 
-function displayResults(results) {
-  hidePlaylists();
-  const artistImage = document.getElementById("artist-img");
-  const artistName = document.getElementById("artist-name");
+function displayResults(result, searchTerm) {
+    resultPlaylist.classList.add("hidden");
+    const gridContainer = document.querySelector('.grid-container');
+    gridContainer.innerHTML = ''; // Limpa os resultados anteriores    
 
-  results.forEach((element) => {
-    artistImage.src = element.urlImg;
-    artistName.innerText = element.name;
-  });
-  resultArtist.classList.remove("hidden");
+    const filteredArtists = result.filter(artist => artist.name.toLowerCase().includes(searchTerm));
+
+    if (filteredArtists.length > 0) {
+        const artist = filteredArtists[0]; // Pega apenas o primeiro resultado filtrado
+        const artistCard = document.createElement('div');
+        artistCard.classList.add('artist-card');
+
+        artistCard.innerHTML = `
+            <div class="card-img">
+                <img class="artist-img" src="${artist.urlImg}" />
+                <div class="play">
+                    <span class="fa fa-solid fa-play"></span>
+                </div>
+            </div>
+            <div class="card-text">              
+                <span class="artist-name">${artist.name}</span>
+                <span class="artist-categorie">Artista</span>
+            </div>
+        `;
+        gridContainer.appendChild(artistCard);
+        resultArtist.classList.remove('hidden');
+    } else {
+        resultArtist.classList.add('hidden');
+    }
 }
 
-function hidePlaylists() {
-  playlistContainer.classList.add("hidden");
-}
+document.addEventListener('input', function () {
+    const searchTerm = searchInput.value.toLowerCase().trim();
 
-searchInput.addEventListener("input", function () {
-  const searchTerm = searchInput.value.toLowerCase();
-  if (searchTerm === "") {
-    resultArtist.classList.add("hidden");
-    playlistContainer.classList.remove("hidden");
-    return;
-  }
-  requestApi(searchTerm);
+    if (searchTerm === '') {
+        resultPlaylist.classList.remove('hidden');
+        resultArtist.classList.add('hidden');
+        return;
+    }
+
+    requestApi(searchTerm);
 });
